9e8a9e5003d3f8e0d94d265725b11e098b11fb68
14harish <harishpc2003@gmail.com>
2022-11-13 18:20:18 +0530

    new

--- toogel.js
+++ toogel.js
@@ -1,1 +1,21 @@
 // console.log("hello");
+const styleSwitchToggle=document.querySelector(".style-switcher-toggler");
+styleSwitchToggle.addEventListener("click",()=>{
+    document.querySelector(".style-switcher").classList.toggle("open"); 
+})
+window.addEventListener("scroll",()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open"); 
+    }
+})
+
+var alternateStyle=document.querySelectorAll(".alternate-style");
+function setActively(color){
+    alternateStyle.forEach((style)=>{
+        if(color===style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
