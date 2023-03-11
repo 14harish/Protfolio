9d2001b95626ef775a6e38bb506a856c1ce905d1
14harish <harishpc2003@gmail.com>
2023-03-11 18:38:40 +0530

    new

--- toogel.js
+++ toogel.js
@@ -1,4 +1,3 @@
-// console.log("hello");
 const styleSwitchToggle=document.querySelector(".style-switcher-toggler");
 styleSwitchToggle.addEventListener("click",()=>{
     document.querySelector(".style-switcher").classList.toggle("open"); 
@@ -18,4 +17,52 @@ function setActively(color){
             style.setAttribute("disabled","true");
         }
     })
+}
+var c=1;
+let pro1=document.getElementById("p1");
+let pro2=document.getElementById("p2");
+let pro3=document.getElementById("p3");
+function rightBtn(){
+    c++;
+    if (c==4){
+        c=1;
+    }
+    if (c==0){
+        c=4;
+    }
+    if(c==1){
+        pro3.classList.add("com");
+        pro1.classList.remove("com");
+        // console.log("project 1");
+    }else if(c==2){
+        pro1.classList.add("com");
+        pro2.classList.remove("com");
+        // console.log("project 2");
+    }else if(c==3){
+        pro2.classList.add("com");
+        pro3.classList.remove("com");
+        // console.log("project 3");
+    }
+}
+function LeftBtn(){    
+    c--    
+    if (c==4){
+        c=1;
+    }
+    if (c==0){
+        c=4;
+    }
+    if(c==1){
+        pro2.classList.add("com");
+        pro1.classList.remove("com");
+        // console.log("project 1");
+    }else if(c==2){
+        pro3.classList.add("com");
+        pro2.classList.remove("com");
+        // console.log("project 2");
+    }else if(c==3){
+        pro1.classList.add("com");
+        pro3.classList.remove("com");
+        // console.log("project 3");
+    }
 }
